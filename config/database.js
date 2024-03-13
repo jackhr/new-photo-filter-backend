644a25be29e4ff534dfc7eea88c6fea7d4e12299
jackhr <jc2o@mac.com>
2024-03-12 23:02:16 -0400

    Update database connection settings

--- config/database.js
+++ config/database.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL)
-  .catch(error => {
-    console.error(error);
-    throw new Error(error);
-  });
+mongoose.connect(process.env.DATABASE_URL, {
+    dbName: process.env.DATABASE_NAME,
+    autoIndex: process.env.NODE_ENV !== 'production', // Disable autoIndex in production
+    serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
+    socketTimeoutMS: 45000,
+    maxPoolSize: 10,
+})
+    .catch(error => {
+        console.error(error);
+        throw new Error(error);
+    });
 
 const db = mongoose.connection;
 
 db.on('connected', function () {
-  console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
+    console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
 });
